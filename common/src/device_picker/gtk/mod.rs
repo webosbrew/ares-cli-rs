c22f18fb40f100513ca377275437e51a70352eb2
Ningyuan Li <mariotaku.lee@gmail.com>
2023-05-08 21:58:07 +0900

    device picker with gtk

---
+++ common/src/device_picker/gtk/mod.rs
@@ -0,0 +1,84 @@
+use std::sync::{Arc, Mutex};
+
+use gtk::{Align, Application, ApplicationWindow, Button, Label, ListBox, ListBoxRow, Orientation};
+use gtk::prelude::*;
+
+use crate::device_manager::Device;
+use crate::device_picker::PickPrompt;
+
+#[derive(Default)]
+pub struct PickPromptGtk {}
+
+
+impl PickPrompt for PickPromptGtk {
+    fn pick<D: AsRef<Device>>(&self, devices: Vec<D>) -> Option<Device> {
+        let app = Application::new(Some("com.ares.devicePickPrompt"), Default::default());
+        let items: Vec<Device> = devices.iter().map(|d| d.as_ref().clone()).collect();
+        let result_index: Arc<Mutex<i32>> = Arc::new(Mutex::new(-1));
+        let ui_result = result_index.clone();
+        app.connect_activate(move |app| {
+            let window = Arc::new(ApplicationWindow::new(app));
+            let ui_selected: Arc<Mutex<i32>> = Arc::new(Mutex::new(-1));
+
+            window.set_title("Select Device");
+            window.set_border_width(10);
+            window.set_position(gtk::WindowPosition::Center);
+            window.set_default_size(400, 300);
+            window.set_resizable(false);
+            window.set_keep_above(true);
+
+            let content = gtk::Box::new(Orientation::Vertical, 5);
+
+            let list = ListBox::new();
+            for item in &items {
+                let row = ListBoxRow::new();
+                let label = Label::new(Some(&item.name));
+                label.set_halign(Align::Start);
+                row.set_child(Some(&label));
+                list.insert(&row, -1);
+            }
+            list.set_vexpand(true);
+            let index = ui_selected.clone();
+            list.connect_row_selected(move |_, selected| {
+                *index.lock().unwrap() = selected.map(|row| row.index()).unwrap_or(-1);
+            });
+
+            content.add(&list);
+
+            let buttons = gtk::Box::new(Orientation::Horizontal, 5);
+            let ok = Button::with_label("OK");
+            let cancel = Button::with_label("Cancel");
+
+            buttons.add(&ok);
+            buttons.add(&cancel);
+            buttons.set_valign(Align::End);
+            buttons.set_halign(Align::End);
+
+            let win = window.clone();
+            let index = ui_selected.clone();
+            let ui_result = ui_result.clone();
+            ok.connect_clicked(move |_| {
+                *ui_result.lock().unwrap() = *index.lock().unwrap();
+                win.close();
+            });
+
+            let win = window.clone();
+            cancel.connect_clicked(move |_| {
+                win.close();
+            });
+
+            content.add(&buttons);
+
+            window.add(&content);
+
+            window.show_all();
+        });
+        app.run_with_args::<&str>(&[]);
+
+        let index = result_index.lock().unwrap().clone();
+        if index < 0 {
+            return None;
+        }
+        return devices.get(index as usize).map(|v| v.as_ref().clone());
+    }
+}
--- common/src/device_picker/mod.rs
+++ common/src/device_picker/mod.rs
@@ -1,11 +1,16 @@
-use std::io::{Error, ErrorKind};
+use std::io::Error;
 
 use crate::device_manager::{Device, DeviceManager};
+#[cfg(all(not(target_os = "windows"), not(target_os = "macos")))]
+use crate::device_picker::gtk::PickPromptGtk;
 #[cfg(target_os = "windows")]
 use crate::device_picker::windows::PickPromptWindows;
 
 #[cfg(target_os = "windows")]
 mod windows;
+
+#[cfg(all(not(target_os = "windows"), not(target_os = "macos")))]
+mod gtk;
 
 pub trait PickDevice {
     fn pick<S: AsRef<str>>(&self, name: Option<S>, pick: bool) -> Result<Option<Device>, Error>;
@@ -18,13 +23,8 @@ impl PickDevice for DeviceManager {
             devices.iter().find(|d| d.name == s.as_ref()).cloned()
         } else if !pick {
             devices.iter().find(|d| d.default.unwrap_or(false)).cloned()
-        } else if cfg!(windows) {
-            PickPromptWindows::default().pick(devices)
         } else {
-            return Err(Error::new(
-                ErrorKind::Unsupported,
-                "This system doesn't support device picker",
-            ));
+            pick_prompt().pick(devices)
         };
         return Ok(device);
     }
@@ -32,4 +32,15 @@ impl PickDevice for DeviceManager {
 
 trait PickPrompt: Default {
     fn pick<D: AsRef<Device>>(&self, devices: Vec<D>) -> Option<Device>;
+}
+
+struct PickPromptPlatform {}
+
+fn pick_prompt() -> impl PickPrompt {
+    #[cfg(target_os = "windows")]
+    return PickPromptWindows::default();
+    #[cfg(target_os = "macos")]
+    todo!("macOS is not yet supported");
+    #[cfg(all(not(target_os = "windows"), not(target_os = "macos")))]
+    return PickPromptGtk::default();
 }
