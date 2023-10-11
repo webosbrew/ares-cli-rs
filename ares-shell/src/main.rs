fcdcf60a5da6557343d9afcc168d3821961a9167
Ningyuan Li <mariotaku.lee@gmail.com>
2023-10-11 14:16:37 +0900

    dumb shell support

--- ares-shell/src/main.rs
+++ ares-shell/src/main.rs
@@ -1,20 +1,14 @@
-use std::io::Write;
-use std::io::{stdout, Read};
+use std::io::stdout;
 use std::process::exit;
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
 
 use clap::Parser;
-use crossbeam_channel::internal::SelectHandle;
-use crossbeam_channel::unbounded;
-use crossterm::event::Event;
+use crossterm::terminal;
 use crossterm::tty::IsTty;
-use crossterm::{terminal, Command};
 
 use ares_connection_lib::session::NewSession;
 use ares_device_lib::DeviceManager;
 
+mod dumb;
 mod pty;
 
 #[derive(Parser, Debug)]
@@ -41,19 +35,19 @@ fn main() {
     let manager = DeviceManager::default();
     let Some(device) = manager.find_or_default(cli.device).unwrap() else {
         eprintln!("Device not found");
-        exit(1);
+        exit(255);
     };
 
     let session = device.new_session().unwrap();
     let ch = session.new_channel().unwrap();
     ch.open_session().unwrap();
-    let (width, height) = terminal::size().unwrap_or((80, 24));
     let mut has_pty = false;
     if !cli.no_pty && (cli.pty || stdout().is_tty()) {
+        let (width, height) = terminal::size().unwrap_or((80, 24));
         if let Err(e) = ch.request_pty("xterm", width as u32, height as u32) {
             eprintln!("Can't request pty: {:?}", e);
             if cli.pty {
-                exit(1);
+                exit(255);
             }
         } else {
             has_pty = true;
@@ -64,44 +58,13 @@ fn main() {
     } else {
         ch.request_shell().unwrap();
     }
-    session.set_blocking(false);
-    if has_pty {
-        terminal::enable_raw_mode().unwrap();
-    }
-    let (tx, rx) = unbounded::<Event>();
-    let terminated = Arc::new(Mutex::new(false));
-    let thread_terminated = Arc::downgrade(&terminated);
-    let join = thread::spawn(move || loop {
-        if let Some(terminated) = thread_terminated.upgrade() {
-            if *terminated.lock().unwrap() {
-                break;
-            }
-        } else {
-            break;
-        }
-        let Ok(has_event) = crossterm::event::poll(Duration::from_millis(20)) else {
-            break;
-        };
-        if !has_event {
-            continue;
-        }
-        let Ok(event) = crossterm::event::read() else {
-            break;
-        };
-        if !tx.send(event).is_ok() {
-            break;
-        }
-    });
-
-    let result = pty::shell(&ch, rx, has_pty);
-    if has_pty {
-        terminal::disable_raw_mode().unwrap();
-    }
-    *terminated.lock().unwrap() = true;
-    join.join().unwrap();
-    session.set_blocking(true);
+    let result = if has_pty {
+        pty::shell(ch)
+    } else {
+        dumb::shell(ch)
+    };
     match result {
-        Ok(_) => {}
+        Ok(code) => exit(code),
         Err(e) => {
             eprintln!("Error: {:?}", e);
         }
--- ares-shell/src/pty.rs
+++ ares-shell/src/pty.rs
@@ -1,17 +1,20 @@
 use std::io::Write;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::thread::JoinHandle;
+use std::time::Duration;
 
-use crossbeam_channel::{select, Receiver};
-use crossterm::event::{Event, KeyCode, KeyEventKind};
-use libssh_rs::Channel;
+use crossbeam_channel::{select, unbounded, Sender};
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind};
+use crossterm::terminal;
 use libssh_rs::Error::TryAgain;
+use libssh_rs::{Channel, Error};
 
-pub(crate) fn shell(
-    ch: &Channel,
-    rx: Receiver<Event>,
-    has_pty: bool,
-) -> Result<(), std::io::Error> {
+pub(crate) fn shell(ch: Channel) -> Result<i32, std::io::Error> {
+    terminal::enable_raw_mode().unwrap();
+    let (tx, rx) = unbounded::<Event>();
+    let events = EventThread::new(tx);
     let mut buf = [0; 1024];
-    let mut stderr = false;
     loop {
         if ch.is_eof() {
             break;
@@ -22,106 +25,153 @@ pub(crate) fn shell(
                     if key.kind == KeyEventKind::Release {
                         continue;
                     }
-                    let mut stdin = ch.stdin();
-                    match key.code {
-                        KeyCode::Backspace => {
-                            stdin.write_all(&[0x08, 0x20, 0x08])?;
-                        }
-                        KeyCode::Enter => {
-                            stdin.write_all(&[0xd])?;
-                        }
-                        KeyCode::Left => {
-                            stdin.write_all(&[0x1b, 0x5b, 0x44])?;
-                        }
-                        KeyCode::Right => {
-                            stdin.write_all(&[0x1b, 0x5b, 0x43])?;
-                        }
-                        KeyCode::Up => {
-                            stdin.write_all(&[0x1b, 0x5b, 0x41])?;
-                        }
-                        KeyCode::Down => {
-                            stdin.write_all(&[0x1b, 0x5b, 0x42])?;
-                        }
-                        KeyCode::Home => {
-                            stdin.write_all(&[0x1b, 0x5b, 0x48])?;
-                        }
-                        KeyCode::End => {
-                            stdin.write_all(&[0x1b, 0x5b, 0x46])?;
-                        }
-                        KeyCode::PageUp => {
-                            stdin.write_all(&[0x1b, 0x5b, 0x35, 0x7e])?;
-                        }
-                        KeyCode::PageDown => {
-                            stdin.write_all(&[0x1b, 0x5b, 0x36, 0x7e])?;
-                        }
-                        KeyCode::Tab => {
-                            stdin.write_all(&[0x09])?;
-                        }
-                        KeyCode::BackTab => {
-                            stdin.write_all(&[0x1b, 0x5b, 0x5a])?;
-                        }
-                        KeyCode::Delete => {
-                            stdin.write_all(&[0x1b, 0x5b, 0x33, 0x7e])?;
-                        }
-                        KeyCode::Insert => {
-                            stdin.write_all(&[0x1b, 0x5b, 0x32, 0x7e])?;
-                        }
-                        KeyCode::F(n) => {
-                            stdin.write_all(&[0x1b, 0x5b, 0x4f, 0x30 + n as u8])?;
-                        }
-                        KeyCode::Char(c) => {
-                            if key.modifiers.contains(crossterm::event::KeyModifiers::CONTROL) {
-                                stdin.write_all(&[c as u8 - 0x61])?;
-                            } else if key.modifiers.contains(crossterm::event::KeyModifiers::ALT) {
-                                stdin.write_all(&[0x1b, c as u8])?;
-                            } else {
-                                stdin.write_all(&[c as u8])?;
-                            }
-                        }
-                        KeyCode::Null => {
-                            stdin.write_all(&[0])?;
-                        }
-                        KeyCode::Esc => {
-                            stdin.write_all(&[27])?;
-                        }
-                        _ => {}
-                    }
+                    send_key(&mut ch.stdin(), &key)?;
                 }
                 Ok(Event::Resize(width, height)) => {
                     ch.change_pty_size(width as u32, height as u32)?;
                 }
                 Ok(e) => {
                 }
-                Err(e) => {
+                Err(_) => {
                     break;
                 }
             },
             default => {
-                match ch.read_nonblocking(&mut buf, stderr) {
-                    Ok(size) => {
-                        if stderr {
-                            let mut stderr = std::io::stderr();
-                            stderr.write_all(&buf[..size])?;
-                            stderr.flush()?;
-                        } else {
-                            let mut stdout = std::io::stdout();
-                            stdout.write_all(&buf[..size])?;
-                            stdout.flush()?;
+                match ch.read_nonblocking(&mut buf, false) {
+                     Err(TryAgain) | Ok(0) => {
+                        if ch.is_closed() {
+                            break;
                         }
+                        thread::sleep(Duration::from_millis(1))
                     }
-                    Err(TryAgain) => {}
+                    Ok(size) => {
+                        let mut stdout = std::io::stdout();
+                        stdout.write_all(&buf[..size])?;
+                        stdout.flush()?;
+                    }
                     Err(e) => {
-                        let mut stderr = std::io::stdout();
-                        stderr.write_fmt(format_args!("Error: {:?}\r\n", e))?;
-                        stderr.flush()?;
-                        break;
+                        return Err(std::io::Error::new(std::io::ErrorKind::Other, e.to_string()));
                     }
-                }
-                if !has_pty {
-                    stderr = !stderr;
                 }
             }
         }
     }
+    drop(events);
+    return Ok(ch.get_exit_status().unwrap_or(-1) as i32);
+}
+
+fn send_key<Stdin: Write>(stdin: &mut Stdin, key: &KeyEvent) -> Result<(), Error> {
+    match key.code {
+        KeyCode::Backspace => {
+            stdin.write_all(&[0x08, 0x20, 0x08])?;
+        }
+        KeyCode::Enter => {
+            stdin.write_all(&[0x0d])?;
+        }
+        KeyCode::Left => {
+            stdin.write_all(&[0x1b, 0x5b, 0x44])?;
+        }
+        KeyCode::Right => {
+            stdin.write_all(&[0x1b, 0x5b, 0x43])?;
+        }
+        KeyCode::Up => {
+            stdin.write_all(&[0x1b, 0x5b, 0x41])?;
+        }
+        KeyCode::Down => {
+            stdin.write_all(&[0x1b, 0x5b, 0x42])?;
+        }
+        KeyCode::Home => {
+            stdin.write_all(&[0x1b, 0x5b, 0x48])?;
+        }
+        KeyCode::End => {
+            stdin.write_all(&[0x1b, 0x5b, 0x46])?;
+        }
+        KeyCode::PageUp => {
+            stdin.write_all(&[0x1b, 0x5b, 0x35, 0x7e])?;
+        }
+        KeyCode::PageDown => {
+            stdin.write_all(&[0x1b, 0x5b, 0x36, 0x7e])?;
+        }
+        KeyCode::Tab => {
+            stdin.write_all(&[0x09])?;
+        }
+        KeyCode::BackTab => {
+            stdin.write_all(&[0x1b, 0x5b, 0x5a])?;
+        }
+        KeyCode::Delete => {
+            stdin.write_all(&[0x1b, 0x5b, 0x33, 0x7e])?;
+        }
+        KeyCode::Insert => {
+            stdin.write_all(&[0x1b, 0x5b, 0x32, 0x7e])?;
+        }
+        KeyCode::F(n) => {
+            stdin.write_all(&[0x1b, 0x5b, 0x4f, 0x30 + n])?;
+        }
+        KeyCode::Char(c) => {
+            if key
+                .modifiers
+                .contains(crossterm::event::KeyModifiers::CONTROL)
+            {
+                stdin.write_all(&[c as u8 - 0x61])?;
+            } else if key.modifiers.contains(crossterm::event::KeyModifiers::ALT) {
+                stdin.write_all(&[0x1b, c as u8])?;
+            } else {
+                stdin.write_all(&[c as u8])?;
+            }
+        }
+        KeyCode::Null => {
+            stdin.write_all(&[0x0])?;
+        }
+        KeyCode::Esc => {
+            stdin.write_all(&[0x1b])?;
+        }
+        _ => {}
+    }
     return Ok(());
+}
+
+struct EventThread {
+    handle: Mutex<Option<JoinHandle<()>>>,
+    terminated: Arc<Mutex<bool>>,
+}
+
+impl EventThread {
+    fn new(tx: Sender<Event>) -> Self {
+        let terminated = Arc::new(Mutex::new(false));
+        let thread_terminated = Arc::downgrade(&terminated);
+        Self {
+            terminated,
+            handle: Mutex::new(Some(thread::spawn(move || loop {
+                if let Some(terminated) = thread_terminated.upgrade() {
+                    if *terminated.lock().unwrap() {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+                let Ok(has_event) = crossterm::event::poll(Duration::from_millis(20)) else {
+                    break;
+                };
+                if !has_event {
+                    continue;
+                }
+                let Ok(event) = crossterm::event::read() else {
+                    break;
+                };
+                if !tx.send(event).is_ok() {
+                    break;
+                }
+            }))),
+        }
+    }
+}
+
+impl Drop for EventThread {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().unwrap();
+        *self.terminated.lock().unwrap() = true;
+        if let Some(hnd) = self.handle.lock().unwrap().take() {
+            hnd.join().unwrap();
+        }
+    }
 }
